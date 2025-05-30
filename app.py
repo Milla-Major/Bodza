c85cb4c24fbdca1cdef1db5c44e066a36ceb4bf8
Milla-Major <milla.major@student.nhlstenden.com>
2025-05-30 13:48:51 +0200

    Set session cookie name in Flask app configuration

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config['SESSION_COOKIE_NAME'] = 'session'
 Session(app)
 
 db = Database('data/magazine_subscription.db')
